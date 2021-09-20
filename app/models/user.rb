6c0f3ebdaf9faf6b7b41fc614882ea03d6ce2609
RisaKubota <89505889+RisaKubota@users.noreply.github.com>
2021-09-20 17:21:09 +0900

    devise

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :name1
+    validates :name2
+    validates :furigana1
+    validates :furigana2
+    validates :birthday
+  end
+end
